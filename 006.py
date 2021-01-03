fca34cf35b160cae6cec867f5ad9c01e2eca7b16
“Javier <frjavaro87@gmail.com>
2021-01-03 14:15:56 -0600

    dfdf

--- 006.py
+++ 006.py
@@ -2,22 +2,24 @@
 
 a = input()
 b = input()
-#c = a + b
+
+c = a + b
+d = a * b
+e = a - b
+f = a / b
+
 # suma
-print(f' la suma es: {a + b}')
-
+print(f' la suma es: {c}')
 # se usa la f para formatear sin necesidad de usar str o int
-
 # resta
-print(f' la resta es: {c}')
-
+print(f' la resta es: {e}')
 #se usan las llaves para reducir la linea de codigo, la comilla después de las llaves
-
+#e = int(a * b)
 # mult
 #print(a * b)
-print(f' la mult es: {a * b}')
+print(f' la mult es: {e}')
 
 # en caso de que se pongan textos en una operación, es cuando se tienen problemas de sintaxis
+f = int(a / b)
 #división
-print(f' la div es: {a / b}')
-
+print(f' la div es: {f}')
