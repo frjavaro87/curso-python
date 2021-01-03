00b5dc3c2602025399fc9d6aa1fb966ae7738e71
“Javier <frjavaro87@gmail.com>
2021-01-03 02:41:20 -0600

    div

--- 002.py
+++ 002.py
@@ -19,4 +19,5 @@ print(f' la mult es: {a * b}')
 
 # en caso de que se pongan textos en una operación, es cuando se tienen problemas de sintaxis
 #división
-#print("la div es:" (a / b))
+print(f' la div es: {a / b}')
+
