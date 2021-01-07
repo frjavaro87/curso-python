b728af17b3c4ba308543f5bbe6d27d980b89e6f8
“Javier <frjavaro87@gmail.com>
2021-01-06 22:44:37 -0600

    nuev

---
+++ comparacion.py
@@ -0,0 +1,18 @@
+# solicitar 2 numeros y mostrar cual es menor. Si son iguales, que lo imprima.
+
+print("Hola, ingresa 2 números")
+
+a = float(input())
+b = float(input())
+
+if a < b:
+    print("El primero es menor")
+    elif a > b: 
+        print("el segundo es menor")
+else:
+    print("son iguales")
+
+
+
+
+
