b52e39561b14e3ea35bf80dd679e466fdbc07880
“Javier <frjavaro87@gmail.com>
2021-01-06 21:59:48 -0600

    premio

---
+++ premio.py
@@ -0,0 +1,12 @@
+# solicitar un numero. si es 1000, imprimir ganaste un premio
+
+print("Hola, escribe un número")
+
+premio = float(input())
+
+if premio == 1000:
+    print("Ganaste un premio")
+else:
+    print("Intenta de nuevo")
+
+    
