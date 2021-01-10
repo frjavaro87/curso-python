690f04773c624635d7fcd4ccca4ee7da297f9a2b
“Javier <frjavaro87@gmail.com>
2021-01-10 00:50:31 -0600

    pensando

--- nominas2.py
+++ nominas2.py
@@ -4,12 +4,17 @@ def impuestos(pago):
 
     return pago * 0.15
 
-def nomina(horas, pago):
+def nomina():
 
+    horas = float(imput("Dame horas: "))
+    pago = float(input("Dame el costo: "))
     bruto = horas * pago
     imp = impuestos(bruto)
     pago_neto = bruto - imp
 
+if horas > 40:
+    extra_time = pago * 1.5
+    print("Bruto es de: ", bruto, "neto es de: ", )
     print("El bruto es de: ", bruto, " y el neto es de: ", pago_neto)
 
-nomina(23, 34)
+nomina()
