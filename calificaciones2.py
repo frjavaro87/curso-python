51f802cce5902a0af42050b08d182f0062ac66f8
“Javier <frjavaro87@gmail.com>
2021-01-10 01:16:13 -0600

    calis2

---
+++ calificaciones2.py
@@ -0,0 +1,19 @@
+# calificaciones con funciones
+
+def score(calif):
+
+    if (calif >= 10.0 and calif <= 0.0):
+        print("La calificación es errónea")
+    else:
+        if(calif >= 9):
+            print("A")
+        elif(calif >= 8.0 and calif <= 9.0):
+            print("B")
+        elif(calif >= 7.0 and calif <= 8.0):
+            print("C")
+        elif(calif >= 6.0 and calif <= 7.0):
+            print("D")
+        else:
+            print("F")
+
+score(7)
