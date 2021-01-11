7b0e653f73d841ea189487d957279fad1561545c
“Javier <frjavaro87@gmail.com>
2021-01-10 20:10:53 -0600

    nuevos

--- lista.py
+++ lista.py
@@ -1,4 +1,6 @@
 # agregando elementos a listas vacías
+#max()
+#min()
 
 lista = []
 entrada = 0
@@ -6,9 +8,15 @@ contador = 0
 
 while entrada != 'Termina':
     entrada = input("Favor de ingresar número o Termina: ")
-    if entrada == 'Total':
-        print("Proceso terminado")
+    if entrada == 'Termina':
+        print(f'Proceso terminado, el máximo es: {max(lista)} ')
+        print(f'Proceso terminado, el mínimo es: {min(lista)} ')
+        print(f"Las interaciones fueron: {contador} ")
+        
+        #print("Tu lista es: ", lista)
+
         break
     else:
         lista.append(int(entrada))
-        print(lista)
+        contador+= 1
+        
