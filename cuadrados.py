88e5829cc6ffbe3a6dd2e8fd3cb6101d6c375389
“Javier <frjavaro87@gmail.com>
2021-01-16 17:29:23 -0600

    cubos

---
+++ cuadrados.py
@@ -0,0 +1,9 @@
+#cuadrados
+
+cuadrado = []
+
+for d in range(100):
+    cuadrado.append(d ** 2)
+
+print(cuadrado)
+
---
+++ cubos.py
@@ -0,0 +1,9 @@
+#cubos de un númoro
+
+datos = []
+
+for y in range(1200):
+
+    datos.append(y ** 3)
+
+print(datos)
