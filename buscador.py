56c15395a78b7c639661c31d4e0d59ac02ea5429
“Javier <frjavaro87@gmail.com>
2021-01-16 19:19:21 -0600

    cisisas

---
+++ buscador.py
@@ -0,0 +1,14 @@
+#búsqueda en textos
+
+# entrada de la palabra
+def palabra():
+    word = input("Qué palabra buscas?: ")
+palabra()
+#entrada del texto predeterminado
+def texto():
+    x = "En una ocasión tuve la oportunidad de hacer las cosas de manera diferente."
+texto()
+#definiendo la función de búsqueda
+def search(word, x):
+    for word in x
+        for 
---
+++ strings.py
@@ -0,0 +1,15 @@
+#manejando strings
+
+x = "Francisco Javier Vazquez Rodriguez"
+
+print(x)
+y = x[0:10]
+print(y)
+z = x[:20]
+print(z)
+w = x[::-1] #reves de string y las posiciones en el string
+print(w)
+
+delta = x.   
+
+print(delta)
