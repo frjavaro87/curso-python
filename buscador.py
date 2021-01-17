c98e79f34ba6098f08059595a894f198ec7fcc7c
“Javier <frjavaro87@gmail.com>
2021-01-17 17:13:45 -0600

    nuev

--- buscador.py
+++ buscador.py
@@ -1,21 +1,17 @@
 #búsqueda en textos
-
 #definiendo la función de búsqueda
-
 def busqueda(texto, keyword):
-
-    descompone = texto.split()
+    descompone = texto.split()      #descompongo el textoen forma de lista
     print(descompone)
     #text.rstip(",.;:")
-    for palabra in descompone:
+    for palabra in descompone:      #inicio el barrido de la palabra en la lista
         print(palabra)
-        if palabra == keyword:
-            print("alabra encontrdad")
-            break
+        if palabra == keyword:      #comparo cada palabra con la palabra clave
+            print("Palabra encontrada") #resultado de la búsqueda
+            break                       #sirve para dejar de imprimir el resultado
         else:
-            pass
-
-tex = input("Ingresa el texto a buscar: ")
-search = input("Ingresa tu palabra a buscar: ")
+            pass                        #si no encuentra la palabra, se detiene
+tex = input("Ingresa el texto a buscar: ")  # texto a estudiar  
+search = input("Ingresa tu palabra a buscar: ") #palabra clave
 
 busqueda(tex,search)
