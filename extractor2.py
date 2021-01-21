513a41cbd0c5d8a74419076f833de43f33dddbd5
“Javier <frjavaro87@gmail.com>
2021-01-20 21:35:28 -0600

    extracto2mejorado

--- extractor2.py
+++ extractor2.py
@@ -5,7 +5,7 @@ def extrae(texto):
     b = texto.find(":")             #regresa el valor de posición del caracter buscado
     print(b)             
     if b >= 0:
-        c = texto[b:]
+        c = texto[b:b + 8]
         c = c.strip(" ,;:.!?")  
         c = float(c)
         print(c)
@@ -14,7 +14,7 @@ def extrae(texto):
 
     pandemiatext = "/home/francisco/Proyectos/Curso-python/curso-python/pandemia.txt"
     virus = open(pandemiatext)
-    print(virus.readlines())
+    print(virus.read())
     virus.close()
 
 extrae("virus")
