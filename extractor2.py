b9731b9df95b307f7916a2ddf70bd9c83e5117ac
“Javier <frjavaro87@gmail.com>
2021-01-20 19:27:14 -0600

    yacasi

--- extractor2.py
+++ extractor2.py
@@ -2,20 +2,19 @@
 
 def extrae(texto):
 
-    b = texto.find(":")             #regresa el valor de posición del caracter buscado
+    b = texto.find(":")
+    print(b)             #regresa el valor de posición del caracter buscado
     if b >= 0:
-       # tex.rstrip(",.:;")
-        c = texto[b: b + 10]
+        c = texto[b: b + 100]
         c = c.strip(" ,;:.!?")  
-       
         c = float(c)
         print(c)
     else:
         print("valor no encontrado")
 
-pandemiatext = "/home/francisco/Proyectos/Curso-python/curso-python/pandemia.txt"
-virus = open(pandemiatext)
-print(virus.readline())
-virus.close()
+    pandemiatext = "/home/francisco/Proyectos/Curso-python/curso-python/pandemia.txt"
+    virus = open(pandemiatext)
+    print(virus.readlines())
+    virus.close()
 
-extrae(virus)
+extrae("virus")
