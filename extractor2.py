b148eae2e684ff2e1dfcb86e1a68736028dfe96c
“Javier <frjavaro87@gmail.com>
2021-01-20 23:39:34 -0600

    nuevo

--- extractor2.py
+++ extractor2.py
@@ -1,20 +1,29 @@
-#extractor de un texto con función
+"""extraer texto de un archivo
+para despues tener el lugar del str :"""
 
 def extrae(texto):
 
-    b = texto.find(":")             #regresa el valor de posición del caracter buscado
-    print(b)             
-    if b >= 0:
-        c = texto[b:b + 8]
-        c = c.strip(" ,;:.!?")  
-        c = float(c)
-        print(c)
-    else:
-        print("valor no encontrado")
-
     pandemiatext = "/home/francisco/Proyectos/Curso-python/curso-python/pandemia.txt"
     virus = open(pandemiatext)
-    print(virus.read())
+    #print(virus.read())
+    d = virus.read()
+    print(d)
+    #virus.close()
+
+    for d in virus:
+        b = d.find(":")             #regresa el valor de posición del caracter buscado
+        print(b)             
+        if b >= 0:
+            c = texto[b:b + 8]
+            c = c.strip(" ,;:.!?")  
+            c = float(c)
+            print(c)
+        else:
+            print("valor no encontrado")
+
+    #pandemiatext = "/home/francisco/Proyectos/Curso-python/curso-python/pandemia.txt"
+    #virus = open(pandemiatext)
+    #print(virus.read())
     virus.close()
 
 extrae("virus")
