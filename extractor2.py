07f96e2e3ecbed56c9f2c26adc570645a0c696af
“Javier <frjavaro87@gmail.com>
2021-01-23 18:17:35 -0600

    terminando

--- extractor2.py
+++ extractor2.py
@@ -1,19 +1,12 @@
 """extraer texto de un archivo
 para despues tener el lugar del str :"""
 
-def extrae(texto):
-
-    #pandemiatext = "/home/francisco/Proyectos/Curso-python/curso-python/pandemia.txt"
-    #virus = open(pandemiatext)
-    #print(virus.read())
-    #d = virus.read()
-    #print(d)
-    #virus.close()
+def extrae(texto, palabra):
 
         b = texto.find(":")             #regresa el valor de posición del caracter buscado
         print(b)             
         if b >= 0:
-            c = texto[b:b + 8]
+            c = texto[b:b + 9]
             c = c.strip(" ,;:.!?")  
             c = float(c)
             print(c)
@@ -22,6 +15,6 @@ def extrae(texto):
             
 pandemiatext = "/home/francisco/Proyectos/Curso-python/curso-python/pandemia.txt"
 virus = open(pandemiatext)
-print(virus.read())
-virus.close()
-extrae("virus")
+virus = virus.read()
+#virus.close()
+extrae(virus, "")
