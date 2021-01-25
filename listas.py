ba616b2e4709e0c8a06610991dc83bffb301b69b
“Javier <frjavaro87@gmail.com>
2021-01-24 19:48:48 -0600

    nuevo

---
+++ listas.py
@@ -0,0 +1,22 @@
+#trabajando con listas
+
+#apertura de archivo
+
+#-*- coding: utf-8 -*-
+main = open("/home/francisco/Proyectos/Curso-python/curso-python/ReadingData2.txt")
+text = main.readlines()
+#main.close()
+ 
+#conversión de archivo a linea por linea
+
+listavacia = []
+palabras = []
+
+for line in text:
+    print(line)
+
+    if palabra in text:
+        pass
+    else:
+        y = palabras.append()
+    
