efe21553c4fe660b87a8d5d33fe92b872457c6ed
“Javier <frjavaro87@gmail.com>
2021-01-31 20:42:30 -0600

    mails

--- mailing.py
+++ mailing.py
@@ -1,2 +1,7 @@
-#traer todos los correos del mailing text
+"""traer todos los correos del mailing text que se anexa
+la intención es saber como extraer el texto que necesitemos de una lista de n lineas"""
+
+datamail = open("/home/francisco/Proyectos/Curso-python/curso-python/mailing2.txt")
+mails = datamail.read()
+print(mails)
 
