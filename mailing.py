d12aed6b88105e3cdb987a1130c4026b778d8a45
“Javier <frjavaro87@gmail.com>
2021-02-06 17:23:19 -0600

    sabado

--- mailing.py
+++ mailing.py
@@ -3,5 +3,17 @@ la intención es saber como extraer el texto que necesitemos de una lista de n l
 
 datamail = open("/home/francisco/Proyectos/Curso-python/curso-python/mailing2.txt")
 mails = datamail.read()
-print(mails)
 
+i = 0
+result = []
+subject = 0
+keyword1 = 'From'
+keyword2 = 'Received'
+print(keyword1)
+print(keyword2)
+
+for keyword1 in mails:
+    if keyword1 == 'From':
+       print(encontrado)
+    else:
+       print(2)
